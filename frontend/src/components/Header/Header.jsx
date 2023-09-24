6241f406edce9835a556470dce9bc9fe522c4c02
MarindanyTracy <marindanytracy0@gmail.com>
2023-09-24 10:32:02 +0300

    Change color of header

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -43,7 +43,7 @@ const Header = () => {
 
   const toggleMenu = () => menuRef.current.classList.toggle('show__menu');
   return (
-    <header className="header flex items-center bg-red-200" ref={headerRef}>
+    <header className="header flex items-center bg-emerald-200" ref={headerRef}>
       <div className="container">
         <div className="flex items-center justify-between mt-5 px-5">
           {/* Logo */}
