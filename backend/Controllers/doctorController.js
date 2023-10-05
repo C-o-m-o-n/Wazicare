57d18fee50b6bb15cf90bae8ca800a0f14d5d292
MarindanyTracy <marindanytracy0@gmail.com>
2023-10-05 17:36:31 +0300

    Add doctors controllers

---
+++ backend/Controllers/doctorController.js
@@ -0,0 +1,50 @@
+import Doctor from '../models/DoctorSchema.js';
+
+export const updateDoctor = async(req,res) => {
+  const id = req.params.id;
+
+  try {
+    const updatedDoctor = await Doctor.findByIdAndUpdate(id,{$set:req.body}, {new:true});
+
+    res.status(200).json({success:true, message: "Successfully updated", data: updateDoctor})
+
+  } catch (error) {
+    res.status(500).json({success:false, message: "An error occured while updating, please try again!"})
+  }
+} 
+export const deleteDoctor = async(req,res) => {
+  const id = req.params.id;
+
+  try {
+    const deletedDoctor = await Doctor.findByIdAndDelete(id);
+
+    res.status(200).json({success:true, message: "Successfully deleted"})
+
+  } catch (error) {
+    res.status(500).json({success:false, message: "An error occured while deleting, please try again!"})
+  }
+} 
+export const getSingleDoctor = async(req,res) => {
+  const id = req.params.id;
+
+  try {
+    const doctor = await Doctor.findById(id).select("-password");
+
+    res.status(200).json({success:true, message: "Doctor found", data: doctor})
+
+  } catch (error) {
+    res.status(404).json({success:false, message: "Doctor not found"})
+  }
+} 
+export const getAllDoctors = async(req,res) => {
+  const id = req.params.id;
+
+  try {
+    const doctors = await Doctor.find({}).select("-password");
+
+    res.status(200).json({success:true, message: "Doctors found", data: doctors})
+
+  } catch (error) {
+    res.status(404).json({success:false, message: "Doctors not found"})
+  }
+} 
