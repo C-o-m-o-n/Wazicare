d27f9d910912a606fdefd89d87a6f760c22a7864
MarindanyTracy <marindanytracy0@gmail.com>
2023-10-05 17:46:19 +0300

    Register Doctors Routes

---
+++ backend/Routes/doctor.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import { updateDoctor, deleteDoctor, getAllDoctors, getSingleDoctor } from '../controllers/doctorController.js';
+
+
+const router = express.Router();
+
+router.get('/', getAllDoctors);
+router.get('/:id', getSingleDoctor);
+router.put('/:id', updateDoctor);
+router.delete('/:id',deleteDoctor);
+
+export default router;
--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ import dotenv from 'dotenv';
 import bodyParser from "body-parser";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/user.js";
+import doctorRoutes from "./routes/doctor.js";
 
 dotenv.config();
 const app = express();
@@ -21,6 +22,7 @@ app.use(cors({
 }));
 app.use('/api/auth', authRoutes)
 app.use('/api/users', userRoutes)
+app.use('/api/doctors', doctorRoutes)
 
 //Routes
 app.get('/', (req,res) => {
