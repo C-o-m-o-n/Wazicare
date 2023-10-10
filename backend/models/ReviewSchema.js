ea34f53eff8b45502e939ee418d9ff81e8518b66
MarindanyTracy <marindanytracy0@gmail.com>
2023-10-10 10:54:03 +0300

    Populate therapist with user info from reviews

--- backend/models/ReviewSchema.js
+++ backend/models/ReviewSchema.js
@@ -25,13 +25,13 @@ const reviewSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-// reviewSchema.pre(/^find/, function (next) {
-//   this.populate({
-//     path: 'user',
-//     select: 'name photo'
-//   });
+reviewSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: 'user',
+    select: 'name photo'
+  });
 
-//   next();
-// })
+  next();
+})
 
 export default mongoose.model("Review", reviewSchema);
