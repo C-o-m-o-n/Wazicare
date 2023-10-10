75bc555a05b97563721d6fa4439f0f9692dccf42
MarindanyTracy <marindanytracy0@gmail.com>
2023-10-10 10:49:28 +0300

    Add Review to Therapist

--- backend/Controllers/reviewControllers.js
+++ backend/Controllers/reviewControllers.js
@@ -18,21 +18,27 @@ export const getAllReviews = async(req,res) => {
 
 export const createReview = async (req, res) => {
 
-  if(!req.body.doctor) req.body.doctor = req.params.doctorId;
-  if(!req.body.user) req.body.user = req.params.userId;
+  if (!req.body.doctor) req.body.doctor = req.params.doctorId;
+  if (!req.body.user) req.body.user = req.userId;
 
-  const newReview = new Review(req.body)
+  const newReview = new Review(req.body);
 
   try {
     const savedReview = await newReview.save();
 
-    await Doctor.findByIdAndUpdate(req.body.doctor, {
-      $push: {reviews: savedReview._id}
-    })
+    await Doctor.findByIdAndUpdate(
+      req.body.doctor,
+      { $push: { reviews: savedReview._id } },
+      { new: true }
+    );
 
-    res.status(200).json({success: true, message:'Review submitted', data:savedReview})
+    res.status(200).json({
+      success: true,
+      message: 'Review submitted',
+      data: savedReview
+    });
   } catch (error) {
-    res.status(500).json({success: false, message:error.message})
-    
+    console.error(error); // Log the error for debugging
+    res.status(500).json({ success: false, message: error.message });
   }
-}
+};
